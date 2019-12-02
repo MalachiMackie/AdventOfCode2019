77c56c0d07520dcba9798924ef911bc3d9cb547d
Malachi <malachi.mackie@hill-labs.co.nz>
2019-12-02 16:39:05 +1300

    Day One

---
+++ AdventOfCode/Challenges/DayOneChallenge.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+
+namespace AdventOfCode.Challenges
+{
+    public static class DayOneChallenge
+    {
+        public static int RunChallenge()
+        {
+            int sum = 0;
+
+            foreach (string row in File.ReadAllLines("input/dayOne.txt"))
+            {
+                int mass = int.Parse(row);
+                sum += CalculateFuelForMass(mass);
+            }
+
+            return sum;
+        }
+
+        private static int CalculateFuelForMass(int mass)
+        {
+            var output = mass / 3;
+            output -= 2;
+
+            if(output <= 0)
+            {
+                return 0;
+            }
+
+            output += CalculateFuelForMass(output);
+
+            return output;
+        }
+    }
+}
---
+++ AdventOfCode/Program.cs
@@ -0,0 +1,13 @@
+﻿using AdventOfCode.Challenges;
+using System;
+
+namespace AdventOfCode
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(DayOneChallenge.RunChallenge());
+        }
+    }
+}
