4a3cfdd7f7670747c32acd3c43d95648c670d6af
Malachi Mackie <malachi.mackie@hill-labs.co.nz>
2019-12-05 22:06:10 +1300

    Day Three

--- AdventOfCode/Challenges/DayThreeChallenge.cs
+++ AdventOfCode/Challenges/DayThreeChallenge.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace AdventOfCode.Challenges
 {
@@ -19,12 +18,11 @@ namespace AdventOfCode.Challenges
             return output;
         }
 
-        private static List<(int X, int Y, int distance)> ProcessWire(string[] wire)
+        private static List<(int X, int Y)> ProcessWire(string[] wire)
         {
-            var currentPos = (X: 0, Y: 0, distance: 0);
-            var wirePositions = new List<(int X, int Y, int distance)>();
+            var currentPos = (X: 0, Y: 0);
+            var wirePositions = new List<(int X, int Y)>();
 
-            int counter = 0;
             foreach (string dir in wire)
             {
                 int length = int.Parse(dir.Substring(1));
@@ -55,10 +53,8 @@ namespace AdventOfCode.Challenges
                 }
                 for(int i = 0; i < length; i++)
                 {
-                    counter++;
                     currentPos.X += xChange;
                     currentPos.Y += yChange;
-                    currentPos.distance = counter;
                     wirePositions.Add(currentPos);
                 }
             }
@@ -73,8 +69,8 @@ namespace AdventOfCode.Challenges
         public static void RunChallenge1()
         {
             string[][] input = GetInput();
-            var wire1 = new HashSet<(int X, int Y, int distance)>(ProcessWire(input[0]));
-            var wire2 = new HashSet<(int X, int Y, int distance)>(ProcessWire(input[1]));
+            var wire1 = new HashSet<(int X, int Y)>(ProcessWire(input[0]));
+            var wire2 = new HashSet<(int X, int Y)>(ProcessWire(input[1]));
 
             var intersections = wire1.Where(x => wire2.Contains(x)).ToList();
 
@@ -83,44 +79,32 @@ namespace AdventOfCode.Challenges
 
         }
 
-        public static async Task RunChallenge2()
+        public static void RunChallenge2()
         {
             string[][] input = GetInput();
 
-
             var wire1 = ProcessWire(input[0]);
-            var wire2 = ProcessWire(input[0]);
+            var wire2 = ProcessWire(input[1]);
 
-            var hash1 = new HashSet<(int X, int Y, int distance)>(wire1);
-            var hash2 = new HashSet<(int X, int Y, int distance)>(wire2);
+            var hash1 = new HashSet<(int X, int Y)>(wire1);
+            var hash2 = new HashSet<(int X, int Y)>(wire2);
 
             var intersections = hash1.Where(x => hash2.Contains(x)).ToList();
-
-            var tasks = intersections.Select(x =>
-            {
-                return Task.Run(() =>
-                {
-                    foreach((int X, int Y, int distance) pos in wire1)
-                    {
-                        if(x.X == pos.X && x.Y == pos.Y)
-                        {
-                            return hash1.First(y => y.X == pos.X && y.Y == pos.Y).distance + hash2.First(y => y.X == pos.X && y.Y == pos.Y).distance;
-                        }
-                    }
-                    return int.MaxValue;
-                });
-            }).ToList();
-
-            await Task.WhenAll(tasks);
-
             int distance = int.MaxValue;
 
-            foreach(Task<int> task in tasks)
+            foreach((int X, int Y) intersect in intersections)
             {
-                int dist = await task;
-                if(dist < distance && dist != 0)
+                for(int i = 0; i < wire1.Count; i++)
                 {
-                    distance = dist;
+                    var wire1Pos = wire1[i];
+                    if(intersect == wire1Pos)
+                    {
+                        var intersectDist = (i + 1) + wire2.IndexOf(wire1Pos) + 1;
+                        if(intersectDist < distance)
+                        {
+                            distance = intersectDist;
+                        }
+                    }
                 }
             }
 
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -1,16 +1,15 @@
 ﻿using AdventOfCode.Challenges;
-using System.Threading.Tasks;
 
 namespace AdventOfCode
 {
     public class Program
     {
-        public static async Task Main(string[] args)
+        public static void Main(string[] args)
         {
             //DayOneChallenge.RunChallenge();
             //DayTwoChallenge.RunChallenge1();
             //DayTwoChallenge.RunChallenge2();
-            await DayThreeChallenge.RunChallenge2();
+            DayThreeChallenge.RunChallenge2();
         }
     }
 }
